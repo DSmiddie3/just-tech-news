45ed220e819bd14f5cdc3a72cf07302cc093996c
Dustin Smiddie <101125377+DSmiddie3@users.noreply.github.com>
2022-09-07 21:35:34 -0400

    Updating the app

---
+++ server.js
@@ -0,0 +1,10 @@
+const { appendFile } = require('fs');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
